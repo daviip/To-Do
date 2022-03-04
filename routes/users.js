9e64cfc55fc02073a7f47339435f339a6c0e0105
daviip <daviipadiall@gmail.com>
2022-03-04 11:19:06 +0100

    Resuelve problema de db

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 var express = require('express');
 const { insertMany } = require('../models/User');
 var router = express.Router();
-const Job = require('../models/Job');
 const User = require('../models/User');
 var bcrypt = require('bcryptjs');
 
